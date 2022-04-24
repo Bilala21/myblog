350fe7a8aa17dad0525b2e210972bd2ad8919dd8
Bilala21 <bilal.kalri@gmail.com>
2022-04-24 08:14:05 +0500

    updated

---
+++ resources/views/assets/js/app.js
@@ -0,0 +1,6 @@
+document.getElementById("post_thumbnail").addEventListener("change", function(){
+    document.getElementById("updated_img").src="../assets/images/"+this.files[0].name
+
+  
+})
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/app.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top|fixed-bottom|sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="./index.html">logo</a>
+            <button class="navbar-toggler hidden-lg-up" type="button" data-bs-toggle="collapse"
+                data-bs-target="#nav__bar" aria-controls="nav__bar" aria-expanded="false"
+                aria-label="Toggle navigation"></button>
+            <div class="collapse navbar-collapse justify-content-end" id="nav__bar">
+                <ul class="navbar-nav ">
+                    <li class="nav-item">
+                        <a class="nav-link" href="./auth/register.html">Signup</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./auth/login.html">Signin</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./auth/logout.html">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main>
+        body
+    </main>
+    <!-- SCRIPT FILES -->
+    <script src="/assets/js/app.js"></script>
+</body>
+
+</html>
